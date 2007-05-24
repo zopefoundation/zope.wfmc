28124f29962fcb0534e8b148ed2e634b0dafda13
Brian Sutherland <brian@vanguardistas.net>
2007-05-24 22:35:29 +0000

    Bump the version number to make this a real release, zope.wfmc was never released with zope so we can do this. Also this software is so stable that theres no point in making betas:)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='zope.wfmc',
-      version='3.4.0b1',
+      version='3.4.0',
       url='http://svn.zope.org/zope.wfmc',
       license='ZPL 2.1',
       description='Zope wfmc',
